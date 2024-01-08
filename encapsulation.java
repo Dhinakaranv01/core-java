9640d90df506a7cd72e46f56df6650959bf8f0f8
Dhinakaranv01 <dhinavasudevan@gmail.com>
2024-01-08 20:37:16 +0530

    encapsulation

---
+++ encapsulation.java
@@ -0,0 +1,22 @@
+public class encap{
+    private String name;
+    private int age ;
+    
+
+
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setAge(int newAge){
+        age = newAge;
+    }
+    public String  getName(){
+        return name;
+
+    }
+    public int getAge(){
+        return age;
+    }
+
+}
+
